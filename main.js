5110b42895324e520e7b8f76bc199f2e57f6a68a
Donut5896 <Nutchanokchon5@gmail.com>
2021-01-11 21:36:03 +0700

    11/01/21

--- main.js
+++ main.js
@@ -1,65 +1,6 @@
-// local reviews data
-const reviews = [
-    {
-      id: 1,
-      name: "influencer",
-      job: "web developer",
-      imgfile:
-        "./image/person-1.jpg",
-      text:
-        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    },
-    {
-      id: 2,
-      name: "anna johnson",
-      job: "customer",
-      imgfile:
-        "./image/person-2.jpg",
-      text:
-        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    },
-    {
-      id: 3,
-      name: "peter jones",
-      job: "customer",
-      imgfile:
-        " ./image/person-3.jpg",
-      text:
-        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    },
-    {
-      id: 4,
-      name: "bill anderson",
-      job: "customer",
-      imgfile:
-      "./image/person-4.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    },
-    {
-      id: 5,
-      name: "customer",
-      job: "influencer",
-      imgfile:
-        "./image/person-5.jpg",
-      text:
-        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-    }
-  ];
 
   
-  
-  // select item
-  const imgfile = document.getElementById("person-img");
-  const author = document.getElementById("author");
-  const job = document.getElementById("job");
-  const info = document.getElementById("info");
-  
-  const prevBtn = document.querySelector('.prev-btn');
-  const randomBtn = document.querySelector('.random-btn');
-  const nextBtn = document.querySelector('.next-btn');
-
-/* close links*/
+// close links
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links"); 
@@ -69,7 +10,7 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
-//--------- shopping cart----------//
+//shopping cart
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
@@ -82,6 +23,9 @@ const productsDOM = document.querySelector('.shop-items');
 
 let cart = [];
 let buttonsDOM = [];
+
+
+//--------- shopping cart----------//
 
 //getting the products
 class Products{
@@ -363,53 +307,208 @@ closeBtn.addEventListener("click", function() {
 
 //-------------------Review-----------------------//
  
-  // set starting item
-  let currentItem = 0;
+import reviews from './reviews.js';
+
+  const imgfile = document.getElementById("person-img");
+  const author = document.getElementById("author");
+  const job = document.getElementById("job");
+  const info = document.getElementById("info");
   
-  //load initial item
-  window.addEventListener("DOMContentLoaded", function() {
-    showPerson(currentItem);
-  });
-  
-  // show person based on item
-  
-  function showPerson(person){
-    const item = reviews[person];
-    imgfile.src = item.imgfile;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
+  const prevBtn = document.querySelector('.prev-btn');
+  const randomBtn = document.querySelector('.random-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const Container = document.querySelector('.reviews-container');
+
+
+
+
+  Container.innerHTML = reviews.map( (person, sildeIndex) => {
+    const {id, name, job, imgfile, text} = person;
+    
+    // set position
+    let position = 'next';
+    if(sildeIndex === 0){
+      position = 'active';
+    }
+    if(sildeIndex === reviews.length - 1){
+      position = 'last';
+    }
+    // display review
+    return `
+                <div class="review ${position}">
+                    <div class="image-container">
+                      <img class="img" src="${imgfile}" id="${id}" alt="${name}" >
+                    </div>
+                    <h4 id="author">${name}</h4>
+                    <p id="job">${job}</p>
+                    <p id="info">
+                        ${text}
+                    </p>
+                    
+                </div>`;
+  }).join('');
+
+
+  const startSlider = (type) => {
+    console.log(type);
+    const active = document.querySelector('.active');
+    const last = document.querySelector('.last');
+    let next = active.nextElementSibling;
+    
+    //when run out of next slide
+    if(!next){
+      next = Container.firstElementChild
+    }
+
+    //to remove class
+    active.classList.remove(['active']);
+    last.classList.remove(['last']);
+    next.classList.remove(['next']);
+
+    //previous button
+    if(type === 'prev'){
+        active.classList.add('next');
+        last.classList.add('active');
+        next = last.previousElementSibling;
+        if(!next){
+          next = Container.lastElementChild;
+        }
+        // set new 'next' class
+        next.classList.remove(['next']);
+        next.classList.add(['last']);
+        return;
+
+    }
+
+
+
+    //to add class
+    active.classList.add(['last']);
+    last.classList.add(['next']);
+    next.classList.add(['active']);
   }
 
+
+
+
+nextBtn.addEventListener('click', () => {
+    startSlider();
+  })
+  prevBtn.addEventListener('click',  () => {
+    startSlider('prev');
+  })
+
+ 
+
+
   
-  // show prev person
+ 
+  
+
+/*class Reviews{
+ async getReviews(){
+    try{
+        let result = await fetch('reviews.json');
+        let data = await result.json();
+
+        let reviews = data.items;
+        
+        reviews  = reviews.map(item => {
+            const {name = item.name;
+            const id = item.id;
+            const image = item.imgfile;
+            return {name, id, imagefile}
+            
+        })
+      
+        return reviews
+       
+    }catch(error){
+        console.log(error);
+    }
+  
+  
+  }
+} 
+
+
+
+
+// display person reviews
+ class UserInterface{
+ showPerson(reviews){
+      let person = '';
+      reviews.forEach(review => {
+        person += `
+               <div class="review">
+                    <div class="img-container">
+                    <img src="${review.imgfile}" id="${review.id}" alt="" >
+                    </div>
+                    <h4 id="${review.name}">sara jones</h4>
+                    <p id="${review.job}">UX Designer</p>
+                    <p id="${review.text}">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Maecenas convallis nibh erat, ac sollicitudin quam dictum ullamcorper. 
+                        Vivamus ultricies, ex sit amet consequat iaculis, erat tellus maximus leo, 
+                        ac faucibus purus sapien quis diam. Proin gravida, sapien scelerisque
+                    </p>
+                  
+                </div>`;
+                  
+      })
+      reviewsDOM.innerHTML = person;
+     
+    }
+ 
+}
+
+ console.log(reviewsDOM)
+
+ /*
+   // show prev person
   prevBtn.addEventListener('click', function(){
     currentItem--;
     if(currentItem < 0){
-      currentItem = reviews.length - 1;
+      currentItem = Reviews.length - 1;
     }
     showPerson(currentItem);
   });
-  
+
   // show next person
   
   nextBtn.addEventListener('click', function(){
     currentItem++;
-    if(currentItem > reviews.length - 1){
+    if(currentItem > Reviews.length - 1){
       currentItem = 0;
     }
     showPerson(currentItem);
   });
-  
-  // show random person
+
+
+
+ // show random person
   
   randomBtn.addEventListener('click', function(){
-    currentItem = Math.floor(Math.random() * reviews.length);
-    if(currentItem > reviews.length - 1){
+    currentItem = Math.floor(Math.random() * Reviews.length);
+    if(currentItem > Reviews.length - 1){
       currentItem = 0;
     }
     showPerson(currentItem);
   });
-  
-  
-  
+  */
+
+
+   
+
+ 
+
+  // show person based on item
+  /* document.addEventListener("DOMContentLoaded", () =>  {
+     const ui =  new UserInterface();
+    const reviews = new Reviews();
+
+    reviews.getReviews().then(reviews => {
+      ui.displayPerson(reviews);
+    })
+  }); */
+   
---
+++ reviews.js
@@ -0,0 +1,49 @@
+const reviews = [
+    {
+      id: 1,
+      name: "influencer",
+      job: "web developer",
+      imgfile:
+        "./image/person-1.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "customer",
+      imgfile:
+        "./image/person-2.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "customer",
+      imgfile:
+        " ./image/person-3.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "customer",
+      imgfile:
+      "./image/person-4.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+    {
+      id: 5,
+      name: "customer",
+      job: "influencer",
+      imgfile:
+        "./image/person-5.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    }
+  ];
+
+  export default reviews;
